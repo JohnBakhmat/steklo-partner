09df17bed3b30b5b1de11994e216e44cd5f33ed2
John Bakhmat <johnbakhmat@gmail.com>
2025-03-10 21:26:54 +0300

    fix: smooth scroll height

--- src/components/crm-form.tsx
+++ src/components/crm-form.tsx
@@ -1,6 +1,7 @@
 export default () => {
 	return (
 		<div
+			className="min-h-[200px]"
 			dangerouslySetInnerHTML={{
 				__html: `
             <script data-b24-form="inline/93/2xvrk2" data-skip-moving="true">(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b10635676/crm/form/loader_93.js');</script>
