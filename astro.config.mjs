ea82153232675483e10479c18f74f6c0743fa9b5
John Bakhmat <johnbakhmat@gmail.com>
2025-03-10 10:43:01 +0300

    feat: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,15 @@ import { default as UnoCSS } from "unocss/astro";
 
 import { default as deno } from "@deno/astro-adapter";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		UnoCSS({
-			injectReset: true,
-		}),
-	],
-	experimental: {
-		svg: true,
-	},
-	adapter: deno(),
+    integrations: [UnoCSS({
+        injectReset: true,
+		}), react()],
+    experimental: {
+        svg: true,
+    },
+    adapter: deno(),
 });
