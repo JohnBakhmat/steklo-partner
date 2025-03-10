aa2180cb567a2e16c5587138b76c6c0253fec5b2
John Bakhmat <johnbakhmat@gmail.com>
2025-03-10 17:33:05 +0300

    wip working on survey for desktop

--- src/components/survey.tsx
+++ src/components/survey.tsx
@@ -69,138 +69,142 @@ export const Survey = () => {
 						Заполни простую анкету
 					</h1>
 
-					<h2 className="font-bold text-[21px] mb-4">
-						Что уже монтировал?
-					</h2>
-					<div className="flex flex-col gap-[7px] mb-[50px]">
-						<Controller
-							name="hasMounted.bathroom"
-							control={control}
-							render={({ field }) => (
-								<Checkbox
-									label="Душевые"
-									isChecked={field.value}
-									onCheckedChange={(x) =>
-										setValue(field.name, x)
-									}
+					<div className="grid grid-cols-1 lg:grid-cols-2">
+						<div>
+							<h2 className="font-bold text-[21px] mb-4">
+								Что уже монтировал?
+							</h2>
+							<div className="flex flex-col gap-[7px] mb-[50px]">
+								<Controller
+									name="hasMounted.bathroom"
+									control={control}
+									render={({ field }) => (
+										<Checkbox
+											label="Душевые"
+											isChecked={field.value}
+											onCheckedChange={(x) =>
+												setValue(field.name, x)
+											}
+										/>
+									)}
 								/>
-							)}
-						/>
+
+								<Controller
+									name="hasMounted.shelves"
+									control={control}
+									render={({ field }) => (
+										<Checkbox
+											label="Полки"
+											isChecked={field.value}
+											onCheckedChange={(x) =>
+												setValue(field.name, x)
+											}
+										/>
+									)}
+								/>
+								<Controller
+									name="hasMounted.mirrors"
+									control={control}
+									render={({ field }) => (
+										<Checkbox
+											label="Зеркала"
+											isChecked={field.value}
+											onCheckedChange={(x) =>
+												setValue(field.name, x)
+											}
+										/>
+									)}
+								/>
+								<Controller
+									name="hasMounted.mirrorpanels"
+									control={control}
+									render={({ field }) => (
+										<Checkbox
+											label="Зеркальное панно"
+											isChecked={field.value}
+											onCheckedChange={(x) =>
+												setValue(field.name, x)
+											}
+										/>
+									)}
+								/>
+								<Controller
+									name="hasMounted.glassdividers"
+									control={control}
+									render={({ field }) => (
+										<Checkbox
+											label="Стеклянные перегородки"
+											isChecked={field.value}
+											onCheckedChange={(x) =>
+												setValue(field.name, x)
+											}
+										/>
+									)}
+								/>
+								<Controller
+									name="hasMounted.stairrailing"
+									control={control}
+									render={({ field }) => (
+										<Checkbox
+											label="Лестничные огражения"
+											isChecked={field.value}
+											onCheckedChange={(x) =>
+												setValue(field.name, x)
+											}
+										/>
+									)}
+								/>
+								<Controller
+									name="hasMounted.glasspatios"
+									control={control}
+									render={({ field }) => (
+										<Checkbox
+											label="Стеклянные козырьки"
+											isChecked={field.value}
+											onCheckedChange={(x) =>
+												setValue(field.name, x)
+											}
+										/>
+									)}
+								/>
+							</div>
+						</div>
 
 						<Controller
-							name="hasMounted.shelves"
+							name="hasMeasurementExperience"
 							control={control}
 							render={({ field }) => (
-								<Checkbox
-									label="Полки"
-									isChecked={field.value}
-									onCheckedChange={(x) =>
-										setValue(field.name, x)
-									}
-								/>
-							)}
-						/>
-						<Controller
-							name="hasMounted.mirrors"
-							control={control}
-							render={({ field }) => (
-								<Checkbox
-									label="Зеркала"
-									isChecked={field.value}
-									onCheckedChange={(x) =>
-										setValue(field.name, x)
-									}
-								/>
-							)}
-						/>
-						<Controller
-							name="hasMounted.mirrorpanels"
-							control={control}
-							render={({ field }) => (
-								<Checkbox
-									label="Зеркальное панно"
-									isChecked={field.value}
-									onCheckedChange={(x) =>
-										setValue(field.name, x)
-									}
-								/>
-							)}
-						/>
-						<Controller
-							name="hasMounted.glassdividers"
-							control={control}
-							render={({ field }) => (
-								<Checkbox
-									label="Стеклянные перегородки"
-									isChecked={field.value}
-									onCheckedChange={(x) =>
-										setValue(field.name, x)
-									}
-								/>
-							)}
-						/>
-						<Controller
-							name="hasMounted.stairrailing"
-							control={control}
-							render={({ field }) => (
-								<Checkbox
-									label="Лестничные огражения"
-									isChecked={field.value}
-									onCheckedChange={(x) =>
-										setValue(field.name, x)
-									}
-								/>
-							)}
-						/>
-						<Controller
-							name="hasMounted.glasspatios"
-							control={control}
-							render={({ field }) => (
-								<Checkbox
-									label="Стеклянные козырьки"
-									isChecked={field.value}
-									onCheckedChange={(x) =>
-										setValue(field.name, x)
-									}
-								/>
+								<fieldset className="mb-5 flex flex-col gap-y-[4.5px] text-base">
+									<legend className="font-bold text-[21px] mb-4">
+										Опыт замеров
+									</legend>
+									<RadioButton
+										isActive={field.value === true}
+										label="Да"
+										id="measurements-true"
+										onClick={() => {
+											setValue(
+												"hasMeasurementExperience",
+												true,
+											);
+										}}
+									/>
+									<p>{field.value}</p>
+									<RadioButton
+										isActive={field.value === false}
+										label="Нет"
+										id="measurements-false"
+										onClick={() => {
+											setValue(
+												"hasMeasurementExperience",
+												false,
+											);
+										}}
+									/>
+								</fieldset>
 							)}
 						/>
 					</div>
-
-					<Controller
-						name="hasMeasurementExperience"
-						control={control}
-						render={({ field }) => (
-							<fieldset className="mb-5 flex flex-col gap-y-[4.5px] text-base">
-								<legend className="font-bold text-[21px] mb-4">
-									Опыт замеров
-								</legend>
-								<RadioButton
-									isActive={field.value === true}
-									label="Да"
-									id="measurements-true"
-									onClick={() => {
-										setValue(
-											"hasMeasurementExperience",
-											true,
-										);
-									}}
-								/>
-								<p>{field.value}</p>
-								<RadioButton
-									isActive={field.value === false}
-									label="Нет"
-									id="measurements-false"
-									onClick={() => {
-										setValue(
-											"hasMeasurementExperience",
-											false,
-										);
-									}}
-								/>
-							</fieldset>
-						)}
-					/>
 					<div className="mb-5 flex flex-col gap-y-5 [&_input]:(rounded-[5px] w-full px-5 py-[15px] text-black)">
 						<Controller
 							name="name"
